53fb1aad193a38582f1ec3412473681083e8c738
NKIIB nishikiout <48525357+nkiib@users.noreply.github.com>
2022-10-18 11:16:39 +0900

    URLをGitHub Usercontentsに変更

--- quote/main.js
+++ quote/main.js
@@ -1,3 +1,2 @@
-javascript:(function(d,s){s=d.createElement('process.js');s.src='File URL';d.body.appendChild(s);})(document)
+javascript:(function(d,s){s=d.createElement('process.js');s.src='https://raw.githubusercontent.com/nkiib/Bookmarklet/master/quote/process.js';d.body.appendChild(s);})(document)
 
-//Bookmarklet js URL : https://dl.dropboxusercontent.com/s/s6sj2z0gnpenbau/inyo.js
